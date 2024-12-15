15d1a0aea842f5b1476897eff5dfe9b39f8e9add
Lyubitshka <antwarowska@gmail.com>
2024-12-15 20:22:56 +0100

    next try

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "300489868"
+    serviceWorkerVersion: "678444996"
   }
 });
