335202dc37e5e0c6c3d3f602251a94f29f5972f1
Lyubitshka <antwarowska@gmail.com>
2024-12-15 15:34:50 +0100

    assets fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "242398524"
+    serviceWorkerVersion: "481190517"
   }
 });
