22ab242e59b478ef6ad3eed081633b74c4456680
Lyubitshka <antwarowska@gmail.com>
2024-12-16 12:55:13 +0100

    drawer content not showing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "678444996"
+    serviceWorkerVersion: "142808716"
   }
 });
